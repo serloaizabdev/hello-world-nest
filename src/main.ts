f692e4471bd577462fdee93af08a1bd50d4c39b6
trevorsmeaton <tsmeaton@mac.com>
2021-12-21 12:28:45 +0000

    Changed port to 3000

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = 3000;
+  // const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 bootstrap();
