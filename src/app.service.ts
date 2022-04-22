09493706c2db8e806426fa2e4452467b3e3df3fd
Sergio Loaiza <sergio.loaiza@bairesdev.com>
2022-04-21 19:45:50 -0500

    Added last update message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const version = process.env.MSG || 'Last update 21-abr-2022'; 
     const msg = process.env.MSG || 'wow it works';
-    return `Hello World From NestJS! - ${msg}`;
+    return `Hello World From NestJS! - ${msg} - ${version}`;
   }
 }
